4992b559b55f19845a706f7cfbb37c1ad8cdfb80
Azmi Wahdan <AzmiW@exalt.ps>
2022-09-29 23:40:01 +0300

    update service

--- src/main/java/com/example/soapServer/services/UserService.java
+++ src/main/java/com/example/soapServer/services/UserService.java
@@ -5,6 +5,7 @@ import com.example.soapServer.respositories.UserRepository;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.io.Serializable;
 import java.util.Optional;
 
 @Service
@@ -16,19 +17,25 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    @PostConstruct
+    public void initData() {
+        addUser(1, "Azmi", "Nokia1");
+        addUser(2, "Jihad", "Nokia2");
+        addUser(3, "Hani", "Nokia3");
+        addUser(4, "Firnas", "Nokia4");
+        addUser(5, "Mhmd", "Nokia5");
+
+    }
+
     public User getUser(int id) {
-        Optional<User> optionalUser = userRepository.findById(id);
-        if (optionalUser.isPresent()) return optionalUser.get();
-        return null;
+        Optional<User> user = userRepository.findById(id);
+        return user.orElseGet(() -> new User(00, "non-existing user", "non-device"));
     }
 
     public String addUser(int id, String name, String device) {
         Optional<User> testUser = userRepository.findById(id);
         if (testUser.isPresent()) return "User with id : " + id + " is already exists";
-        User user = new User();
-        user.setId(id);
-        user.setName(name);
-        user.setDevice(device);
+        User user = new User(id, name, device);
         userRepository.save(user);
         return "User with id : " + id + " added successfully";
     }
